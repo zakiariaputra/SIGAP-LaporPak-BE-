0df15fd46192bcd51331c180ace42d68d4330c22
zakiariaputra <zak1ariaputra@gmail.com>
2026-08-19 15:06:20 +0700

    add cloudinary upload provider

--- config/plugins.js
+++ config/plugins.js
@@ -23,6 +23,16 @@ const deniedExecutableTypes = [
 module.exports = ({ env }) => ({
   upload: {
     config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
       sizeLimit: 5 * 1024 * 1024,
     },
   },
