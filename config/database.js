65dc1459c4e0898d90bab01e4864cbdf27aadecf
zakiariaputra <zak1ariaputra@gmail.com>
2026-08-19 11:55:17 +0700

    tes log manual sementara

--- config/database.js
+++ config/database.js
@@ -9,20 +9,23 @@ require('dotenv').config({
   path: path.resolve(process.cwd(), '.env.local'),
   override: true,
 });
-console.log('DB_HOST:', env('DATABASE_HOST'));
-console.log('DB_PORT:', env('DATABASE_PORT'));
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'mysql',
+module.exports = ({ env }) => {
+  console.log('DB_HOST:', env('DATABASE_HOST'));
+  console.log('DB_PORT:', env('DATABASE_PORT'));
+
+  return {
     connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL', true) ? { rejectUnauthorized: false } : false,
+      client: 'mysql',
+      connection: {
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME'),
+        user: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        ssl: env.bool('DATABASE_SSL', true) ? { rejectUnauthorized: false } : false,
+      },
+      useNullAsDefault: true,
     },
-    useNullAsDefault: true,
-  },
-});
+  };
+};
