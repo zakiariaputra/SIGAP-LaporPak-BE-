9c11d76637b4a709f31469b3e6c1cf0465d4f382
zakiariaputra <zak1ariaputra@gmail.com>
2026-08-19 11:00:10 +0700

    update env.local & database.js

--- config/database.js
+++ config/database.js
@@ -1,15 +1,20 @@
 const path = require('path');
 
+require('dotenv').config({
+  path: path.resolve(process.cwd(), '.env.local'),
+  override: true,
+});
+
 module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
+      host: env('DATABASE_HOST'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'sigap_db'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', ''),
-      ssl: env.bool('DATABASE_SSL', false),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: env.bool('DATABASE_SSL', true) ? { rejectUnauthorized: false } : false,
     },
     useNullAsDefault: true,
   },
